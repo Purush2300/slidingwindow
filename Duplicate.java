274ecba719b02fed412f42fa1528148cdc1eae42
purush2300 <reddypurushothm@gmail.com>
2026-02-14 00:05:18 +0530

    Arrays

--- Duplicate.java
+++ Duplicate.java
@@ -1,24 +1,23 @@
+import java.util.HashSet; // Import the HashSet class
+
+
 public class Duplicate {
-    static void dup(int[]a){
+    static boolean dup(int[]a){
+          HashSet<Integer> set = new HashSet<Integer>();
 for (int i = 0; i < a.length; i++) {
-            for (int j = 1; j < a.length; j++) {
-                if(a[i]==a[j]){
-                    System.out.println("true");
-                    break;
-                    
-                }
-               else{
-                System.out.println("false");
-                break;
-               }
-            }
+  
+            
+                if(set.contains(a[i])){
+                    return true;
+                 }
+                 set.add(a[i]);
            
         }
- 
+ return false;
     }
     public static void main(String[] args) {
         int []a={1,2,3,4,1};
-        dup(a);
-       
+        boolean c=dup(a);
+        System.out.println(c);
     }
 }
